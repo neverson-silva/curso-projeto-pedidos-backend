6940901d66c8728677db5bdd34d318bfe74b467b
Neverson Bento da Silva <neverson.silva@kroton.com.br>
2020-05-29 21:15:33 -0300

    Testando classe Categoria

--- src/main/java/com/neversonsilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/neversonsilva/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,27 @@
 package com.neversonsilva.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping("")
-	public String listar() {
+	public List<Categoria> listar() {	
 		
-		return "REST Está funcionando!";
+		var cat1 = new Categoria(1, "Informatica");
+		var cat2 = new Categoria(2	, "Escritório");
+		
+		var lista = Arrays.asList(cat1, cat2, 2);
+			
+		return lista;
 	}
 
 }
