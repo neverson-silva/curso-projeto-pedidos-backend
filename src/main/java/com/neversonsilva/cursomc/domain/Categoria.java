5ef33c3e71537fe88df5fa58725a81aebc64827b
Neverson Bento da Silva <neverson.silva@kroton.com.br>
2020-05-30 11:21:42 -0300

    Add H2 for test and mysql

--- src/main/java/com/neversonsilva/cursomc/domain/Categoria.java
+++ src/main/java/com/neversonsilva/cursomc/domain/Categoria.java
@@ -1,6 +1,12 @@
-package domain;
+package com.neversonsilva.cursomc.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,12 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	@Column
 	private Integer id;
 	
+	@Column
 	private String nome;
-	
 }
--- src/main/java/com/neversonsilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/neversonsilva/cursomc/resources/CategoriaResource.java
@@ -3,23 +3,23 @@ package com.neversonsilva.cursomc.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import com.neversonsilva.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping("")
-	public List<Categoria> listar() {	
+	public List<Categoria> listar() {
 		
-		var cat1 = new Categoria(1, "Informatica");
+		var cat1 = new Categoria(1, "Informaticas");
 		var cat2 = new Categoria(2	, "Escritório");
 		
-		var lista = Arrays.asList(cat1, cat2, 2);
+		var lista = Arrays.asList(cat1, cat2);
 			
 		return lista;
 	}
