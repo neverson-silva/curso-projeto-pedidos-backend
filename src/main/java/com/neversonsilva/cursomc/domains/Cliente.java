c7405a4ce7c42dcce15d4858bc57b7235ddfadcf
Neverson Bento da Silva <neversonbs13@gmail.com>
2020-08-13 14:42:10 -0300

    Ajustando deleçao de clientes

--- src/main/java/com/neversonsilva/cursomc/domains/Cliente.java
+++ src/main/java/com/neversonsilva/cursomc/domains/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -63,7 +64,7 @@ public class Cliente implements Serializable {
 
 	@Getter
 	@Setter
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)
 	@JsonManagedReference
 	private List<Endereco> enderecos = new ArrayList<>();
 	
