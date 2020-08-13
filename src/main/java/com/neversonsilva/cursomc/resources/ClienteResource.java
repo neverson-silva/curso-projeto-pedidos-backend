a250fe30d838956cb07633eb967b4cfbf88b755b
Neverson Bento da Silva <neversonbs13@gmail.com>
2020-08-13 12:26:00 -0300

    GET, PUT e DELETE para Cliente

--- src/main/java/com/neversonsilva/cursomc/resources/ClienteResource.java
+++ src/main/java/com/neversonsilva/cursomc/resources/ClienteResource.java
@@ -1,14 +1,19 @@
 package com.neversonsilva.cursomc.resources;
 
+import com.neversonsilva.cursomc.domains.Cliente;
+import com.neversonsilva.cursomc.dto.ClienteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.neversonsilva.cursomc.domains.Cliente;
 import com.neversonsilva.cursomc.services.ClienteService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 
 @RestController
@@ -25,6 +30,57 @@ public class ClienteResource {
 		
 		return ResponseEntity.ok(cliente);
 			
+	}
+
+	/*@PostMapping("")
+	@ResponseBody
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
+
+		Cliente categoria = clienteService.fromDto(objDto);
+		categoria = clienteService.insert(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	*/
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable("id") Integer id, @Valid @RequestBody ClienteDTO dto) {
+		Cliente categoria = clienteService.fromDto(dto);
+		categoria.setId(id);
+		categoria = clienteService.update(categoria);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@GetMapping("")
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<ClienteDTO> categorias = clienteService.findAll();
+		return ResponseEntity.ok().body(categorias);
+
+	}
+
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction
+	) {
+		Page<Cliente> categorias = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = categorias.map(cat -> new ClienteDTO(cat));
+
+		return ResponseEntity.ok().body(listDto);
+
 	}
 
 }
--- src/main/java/com/neversonsilva/cursomc/services/ClienteService.java
+++ src/main/java/com/neversonsilva/cursomc/services/ClienteService.java
@@ -1,8 +1,17 @@
 package com.neversonsilva.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.neversonsilva.cursomc.domains.Cliente;
+import com.neversonsilva.cursomc.dto.ClienteDTO;
+import com.neversonsilva.cursomc.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.neversonsilva.cursomc.domains.Cliente;
@@ -20,4 +29,49 @@ public class ClienteService {
 		return pedido.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo " + Cliente.class.getName()));
 	}
+
+	public Cliente update(Cliente cliente) {
+
+		Cliente newCliente = this.find( cliente.getId() );
+
+		updateData(newCliente, cliente);
+		
+		return clienteRepository.save(newCliente);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível porque há entidades relacionadas");
+		}
+
+	}
+
+	public List<ClienteDTO> findAll() {
+		List<Cliente> clientes =  clienteRepository.findAll();
+		List<ClienteDTO> clienteDTOList = clientes.stream()
+				.map( cli -> new ClienteDTO( cli) )
+				.collect(Collectors.toList());
+		return clienteDTOList;
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Sort.Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+
+	public Cliente fromDto(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
+	}
+	
+	
+	
 }
