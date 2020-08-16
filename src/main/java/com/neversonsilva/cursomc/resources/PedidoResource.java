ffcf85b734c83515abc4c8efbb7b26d1fc520132
Neverson Bento da Silva <neversonbs13@gmail.com>
2020-08-16 14:30:31 -0300

    criando profile de desenvolvimento

--- src/main/java/com/neversonsilva/cursomc/resources/PedidoResource.java
+++ src/main/java/com/neversonsilva/cursomc/resources/PedidoResource.java
@@ -1,10 +1,13 @@
 package com.neversonsilva.cursomc.resources;
 
+import java.net.URI;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-
 import com.neversonsilva.cursomc.domains.Pedido;
-import com.neversonsilva.cursomc.dto.PedidoDTO;
 import com.neversonsilva.cursomc.services.PedidoService;
 
 
