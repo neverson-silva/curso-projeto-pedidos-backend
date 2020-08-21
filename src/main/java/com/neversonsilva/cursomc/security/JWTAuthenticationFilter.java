92bbc62726620e5c69a30257806acc014c124d4a
Neverson Bento da Silva <neversonbs13@gmail.com>
2020-08-21 09:47:36 -0300

    Expondo o headers cors

--- src/main/java/com/neversonsilva/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/neversonsilva/cursomc/security/JWTAuthenticationFilter.java
@@ -57,7 +57,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             Authentication auth) throws IOException, ServletException {
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
-        res.addHeader("Authorization", "Bearer " + token);
+		res.addHeader("Authorization", "Bearer " + token);
+		res.addHeader("access-control-expose-headers", "Authorization");
 		
 	}
 	
